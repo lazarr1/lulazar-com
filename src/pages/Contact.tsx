67b6e45b63c4561b244c3ca32f02168935120a5a
luke pc <lazarluke02@gmail.com>
2025-12-30 02:09:04 +1100

    changed order of socials

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -11,10 +11,10 @@ export function Socials() {
     <div className="fixed bottom-4 left-1 z-5 px-4 py-5 transition-colors text-sm">
         <div className="mt-12 text-[#808080] opacity-0 animate-fadeInUp delay-400">
             <div className="py-2 px-4 flex-col gap-8 items-center">
-              <GitHubButton />
               <LinkedInButton />
-              <EmailButton />
               <ResumeButton />
+              <EmailButton />
+              <GitHubButton />
             </div>
           </div>
       </div>);
